6dc1c1e6eb638f3fde68aabea90fa3677257d223
satyamsiuu <rawatsatyam058@gmail.com>
2025-08-06 11:36:39 +0530

    work for today is two practicals

---
+++ PCS-502/06_08_2025/p1.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<sys/types.h>
+
+int main()
+{
+    int cpid;
+    cpid = fork();
+    if(cpid>0)
+    {
+        printf("This parent process with PID: %d with Child PID: %d\n",getpid(),cpid);
+        printf("Terminating parent process\n");
+        exit(0);
+    }
+    else if(cpid==0)
+    {
+        sleep(5);
+        printf("This is the orphan child with PID: %d and Parent pid: %d\n",getpid(),getppid());
+    }
+    else
+    {
+        printf("Forking failed\n");
+    }
+    return 0;
+}
---
+++ PCS-502/06_08_2025/p2.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<sys/types.h>
+#include<stdlib.h>
+#include<unistd.h>
+
+int main()
+{
+    int cpid;
+    cpid = fork();
+    if(cpid>0)
+    {
+        printf("This is the parent process PID: %d and child process PID: %d\n",getpid(),cpid);
+        sleep(5);
+    }
+    else if(cpid==0)
+    {
+        printf("This is the zombie child with PID: %d and Parent pid: %d\n",getpid(),getppid());
+        exit(0);
+    }
+    else
+    {
+        printf("Forking failed\n");
+    }
+
+    return 0;
+}
